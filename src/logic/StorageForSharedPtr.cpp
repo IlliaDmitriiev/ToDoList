616c666346f4454f4ae0bfc63ee211eb590a050e
Илля <illia.dmitriiev@teamdev.com>
2020-08-03 10:22:38 +0300

    created storage for all tasks

---
+++ src/logic/StorageForSharedPtr.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Илля on 03.08.2020.
+//
+
+#include "StorageForSharedPtr.h"
+
+void StorageForSharedPtr::addTask(TaskID id, std::shared_ptr<FullTask> sft){
+    allTasks_.insert({id, std::move(sft)});
+}
+
+std::weak_ptr<FullTask> StorageForSharedPtr::getTask(TaskID id){
+    return allTasks_[id];
+}
+
+void StorageForSharedPtr::deleteTask(TaskID id){
+    allTasks_.erase(id);
+}
