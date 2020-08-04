ac6b1ab927330b4066c5ae016b968853a003a145
illia.dmitriiev <illia.dmitriiev@teamdev.com>
2020-08-04 17:23:39 +0300

    added file which tests Cleaner

---
+++ tests/logic/CleanerTest.cpp
@@ -0,0 +1,19 @@
+//
+// Created by illia.dmitriiev on 8/4/2020.
+//
+
+#include <gtest/gtest.h>
+#include "Cleaner.h"
+
+
+class CleanerTest : public ::testing::Test {
+
+};
+
+TEST_F(CleanerTest, shouldTestUpdateFunc) {
+   Cleaner cl;
+    cl.increaseTotalCount(20);
+    ASSERT_FALSE(cl.isItTimeToUpdate());
+    cl.increaseDeletedCount(4);
+    ASSERT_TRUE(cl.isItTimeToUpdate());
+}
