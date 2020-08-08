849b6fee8434b8b138de26e11b6daf4020b9f35c
Илля <illia.dmitriiev@teamdev.com>
2020-08-08 13:42:03 +0300

    added optional [changed return value of function: getTask()]

--- src/logic/StorageForSharedPtr.cpp
+++ src/logic/StorageForSharedPtr.cpp
@@ -8,10 +8,11 @@ void StorageForSharedPtr::addTask(std::shared_ptr<FullTask> sft){
     allTasks_.insert({sft->getId(), std::move(sft)});
 }
 
-std::weak_ptr<FullTask> StorageForSharedPtr::getTask(TaskID id){
+std::optional<std::weak_ptr<FullTask>> StorageForSharedPtr::getTask(TaskID id){
     if (allTasks_.find(id) == allTasks_.end())
-        throw std::runtime_error("");
-    else return allTasks_[id];
+        return std::nullopt;
+    else
+        return allTasks_[id];
 }
 
 void StorageForSharedPtr::deleteTask(TaskID id){
