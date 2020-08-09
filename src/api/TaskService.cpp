82a626d01dfd789059163dc83c402aaa0f1ccf69
Илля <illia.dmitriiev@teamdev.com>
2020-08-09 03:04:26 +0300

    replaced lots of parameters in functions addTask, addSubtask with TaskDTO

--- src/api/TaskService.cpp
+++ src/api/TaskService.cpp
@@ -4,18 +4,18 @@
 
 #include "TaskService.h"
 
-TaskCreationResult TaskService::addTask(Date date, const std::string &name, const std::string &label, Task::Priority prior){
+TaskCreationResult TaskService::addTask(const TaskDTO &taskDto){
     TaskID id = generator_.generateId();
-    auto sft = std::make_shared<FullTask>(FullTask::create(id, Task::create(date, name, label, prior)));
+    auto sft = std::make_shared<FullTask>(FullTask::create(id, Convertor::getTask(taskDto)));
     view_.getViewByD().getStorage().addTask(sft);
     view_.getViewByP().getStorage().putTaskInRightPlace(sft);
     storage_.addTask(std::move(sft));
     return TaskCreationResult::success(id);
 }
 
-TaskCreationResult TaskService::addSubtask(TaskID id, Date date, const std::string &name, const std::string &label, Task::Priority prior){
-    auto subtaskID = addTask(date, name, label, prior);
-    std::optional<std::weak_ptr<FullTask>> ft = storage_.getTask(id);
+TaskCreationResult TaskService::addSubtask(TaskID taskID, const TaskDTO &subTask){
+    auto subtaskID = addTask(subTask);
+    std::optional<std::weak_ptr<FullTask>> ft = storage_.getTask(taskID);
     if(ft.has_value()) {
         if (subtaskID.getTaskID().has_value()) {
             ft.value().lock()->addSubtask(storage_.getTask(subtaskID.getTaskID().value()).value());
@@ -32,7 +32,7 @@ std::vector<TaskDTO> TaskService::getAllTasksByPrior(){
     auto v = view_.getAllTasksByPrior();
     std::vector<TaskDTO> vec;
     for(auto i: v)
-        vec.push_back(convertor_.convert(i));
+        vec.push_back(Convertor::getTaskDTO(i));
 
     return vec;
 }
@@ -41,7 +41,7 @@ std::vector<TaskDTO> TaskService::getTasksForToday(){
     auto v = view_.getTasksForToday();
     std::vector<TaskDTO> vec;
     for(auto i: v)
-        vec.push_back(convertor_.convert(i));
+        vec.push_back(Convertor::getTaskDTO(i));
 
     return vec;
 }
@@ -50,7 +50,7 @@ std::vector<TaskDTO> TaskService::getTasksForWeek(){
     auto v = view_.getTasksForWeek();
     std::vector<TaskDTO> vec;
     for(auto i: v)
-        vec.push_back(convertor_.convert(i));
+        vec.push_back(Convertor::getTaskDTO(i));
 
     return vec;
 };
@@ -63,7 +63,7 @@ std::optional<TaskDTO> TaskService::getTask(TaskID id){
     std::optional<std::weak_ptr<FullTask>> ft = storage_.getTask(id);
     if (ft.has_value())
         return
-            std::optional<TaskDTO>(convertor_.convert(ft.value()));
+            std::optional<TaskDTO>(Convertor::getTaskDTO(ft.value()));
      else
          return std::nullopt;
 }
