85e3a5f057c66b9a376142af943d1677adcff2e9
illia.dmitriiev <illia.dmitriiev@teamdev.com>
2020-08-11 11:59:35 +0300

    merged storage by date into view by date

--- src/logic/ViewByDate.cpp
+++ src/logic/ViewByDate.cpp
@@ -7,20 +7,19 @@
 
 std::vector<std::weak_ptr<FullTask>> ViewByDate::getTasksForToday(Date date){
 
-    auto DataMap = storage_.getMap();
-    auto Node = DataMap.find(date);
+    auto Node = map_.find(date);
     std::vector<std::weak_ptr<FullTask>> v;
 
-    if(Node!=DataMap.end()){
+    if(Node!=map_.end()){
         auto PriorityMap = Node->second.getMap();
 
-        for( auto i: PriorityMap[1])
+        for( auto i: PriorityMap[Task::Priority::First])
             v.push_back(i.second);
-        for( auto i: PriorityMap[2])
+        for( auto i: PriorityMap[Task::Priority::Second])
             v.push_back(i.second);
-        for( auto i: PriorityMap[3])
+        for( auto i: PriorityMap[Task::Priority::Third])
             v.push_back(i.second);
-        for( auto i: PriorityMap[0])
+        for( auto i: PriorityMap[Task::Priority::None])
             v.push_back(i.second);
     }
     return v;
@@ -28,7 +27,6 @@ std::vector<std::weak_ptr<FullTask>> ViewByDate::getTasksForToday(Date date){
 
 std::vector<std::weak_ptr<FullTask>>  ViewByDate::getTasksForWeek(Date date){
 
-    auto map = storage_.getMap();
     std::vector<std::weak_ptr<FullTask>> v;
 
     for (int i = 1; i<=7; ++i) {
@@ -36,23 +34,40 @@ std::vector<std::weak_ptr<FullTask>>  ViewByDate::getTasksForWeek(Date date){
         if (WeekDay == 0) WeekDay = 7;
         date.moveWithinTheWeek(i - WeekDay);
 
-        auto NeedData = map.find(date);
-        if (NeedData != map.end()) {
+        auto NeedData = map_.find(date);
+        if (NeedData != map_.end()) {
             auto PriorityMap = NeedData->second.getMap();
 
-            for( auto i: PriorityMap[1])
+            for( auto i: PriorityMap[Task::Priority::First])
                 v.push_back(i.second);
-            for( auto i: PriorityMap[2])
+            for( auto i: PriorityMap[Task::Priority::Second])
                 v.push_back(i.second);
-            for( auto i: PriorityMap[3])
+            for( auto i: PriorityMap[Task::Priority::Third])
                 v.push_back(i.second);
-            for( auto i: PriorityMap[0])
+            for( auto i: PriorityMap[Task::Priority::None])
                 v.push_back(i.second);
         }
     }
     return v;
 }
 
-StorageByDate &ViewByDate::getStorage() {
-    return storage_;
+void ViewByDate::addTask(const std::weak_ptr<FullTask>& ft){
+    Date date = ft.lock()->getTask().getDate();
+    auto foundDate = map_.find(date);
+
+    if (foundDate != map_.end()) {
+        foundDate->second.addTask(ft);
+    }
+    else {
+        ViewByPriority map;
+        map.addTask(ft);
+        map_.insert({date, map});
+    }
+}
+
+void ViewByDate::deleteTask(const std::weak_ptr<FullTask>& ft){
+    Date date = ft.lock()->getTask().getDate();
+    Task::Priority priority = ft.lock()->getTask().getPrior();
+    map_[date].deleteTask(priority, ft.lock()->getId());
+
 }
