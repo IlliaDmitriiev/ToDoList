548229d5083d000d52a76790500704dc2f590b29
illia <illia.dmitriiev@teamdev.com>
2020-08-13 18:39:50 +0300

    updated ReturnType

--- src/api/Creator.h
+++ src/api/Creator.h
@@ -9,10 +9,26 @@
 
 namespace Creator {
 
-    TaskService createService() {
+    static TaskService createService() {
         auto viewByPriority = std::make_unique<ViewByPriority>();
         auto viewByDate = std::make_unique<ViewByDate>();
         return TaskService(std::move(viewByPriority), std::move(viewByDate));
     }
+
+    static AddTaskResult createAddTaskResult(const ResultType &result,  const std::optional<TaskID> &id, const std::string &error_message){
+        AddTaskResult outcome;
+        outcome.result = result;
+        outcome.id = id;
+        outcome.error_message = error_message;
+        return outcome;
+    }
+
+    static RequstTaskResult createRequestTaskResult(const ResultType &result, const std::string &error_message){
+        RequstTaskResult outcome;
+        outcome.result = result;
+        outcome.error_message = error_message;
+        return outcome;
+    }
+
 }
 #endif //TODOLIST_CREATOR_H
--- src/api/TaskService.cpp
+++ src/api/TaskService.cpp
@@ -3,9 +3,10 @@
 //
 
 #include "TaskService.h"
+#include "Creator.h"
 
 AddTaskResult TaskService::addTask(const TaskDTO &taskDTO){
-    AddTaskResult task_result;
+
     TaskID id = generator_.generateId();
     auto shared_task = std::make_shared<FullTask>(FullTask::create(id, TaskConvertor::transferToTask(taskDTO)));
 
@@ -13,42 +14,30 @@ AddTaskResult TaskService::addTask(const TaskDTO &taskDTO){
     byPriority_->addTask(shared_task);
     storage_.addTask(std::move(shared_task));
 
-    task_result.result = ResultType::SUCCESS;
-    task_result.id = id;
-    return task_result;
+    return Creator::createAddTaskResult(ResultType::SUCCESS, id, "");
 }
 
 AddTaskResult TaskService::addSubtask(TaskID taskID, const TaskDTO &subTask){
-    AddTaskResult task_result;
     auto subtask_result = addTask(subTask);
     std::optional<std::weak_ptr<FullTask>> ft = storage_.getTask(taskID);
 
     if(ft.has_value()) {
         if (subtask_result.id.has_value()) {
             ft.value().lock()->addSubtask(storage_.getTask(subtask_result.id.value()).value());
-            task_result.result = ResultType::SUCCESS;
-            task_result.id = subtask_result.id.value();
-            return task_result;
+            return Creator::createAddTaskResult(ResultType::SUCCESS, subtask_result.id.value(), "");
         }
-        else {
-            task_result.result = ResultType::FAILURE;
-            task_result.id = std::nullopt;
-            task_result.error_message = "subtask creation is failed";
-            return task_result;
-        }
+        else
+            return Creator::createAddTaskResult(ResultType::FAILURE, std::nullopt, "subtask creation is failed");
     }
-    else {
-        task_result.result = ResultType::FAILURE;
-        task_result.id = std::nullopt;
-        task_result.error_message = "task not found";
-        return task_result;
-    }
+    else
+        return Creator::createAddTaskResult(ResultType::FAILURE, std::nullopt, "task not found");
+
 }
 
 std::vector<TaskDTO> TaskService::getAllTasksByPriority(){
     auto v = byPriority_->getAllTasksByPrior();
     std::vector<TaskDTO> vec;
-    for(auto i: v)
+    for(auto &i: v)
         vec.push_back(TaskConvertor::transferToTaskDTO(i));
 
     return vec;
@@ -61,7 +50,7 @@ std::vector<TaskDTO> TaskService::getTasksForToday(){
 
     auto v = byDate_->getTasksForToday(date);
     std::vector<TaskDTO> vec;
-    for(auto i: v)
+    for(auto &i: v)
         vec.push_back(TaskConvertor::transferToTaskDTO(i));
 
     return vec;
@@ -74,17 +63,16 @@ std::vector<TaskDTO> TaskService::getTasksForWeek(){
 
     auto v = byDate_->getTasksForWeek(date);
     std::vector<TaskDTO> vec;
-    for(auto i: v)
+    for(auto &i: v)
         vec.push_back(TaskConvertor::transferToTaskDTO(i));
 
     return vec;
 };
 
 RequstTaskResult TaskService::deleteTask(TaskID id){
-    RequstTaskResult task_result;
     auto task = storage_.getTask(id);
-    if(task.has_value()) {
 
+    if(task.has_value()) {
         RequstTaskResult subtask_results;
         subtask_results.result = ResultType::SUCCESS;
 
@@ -94,19 +82,14 @@ RequstTaskResult TaskService::deleteTask(TaskID id){
                 subtask_results = subtask_result;
         }
 
-        if(!removeTask(task.value().lock())) {
-            task_result.result = ResultType::FAILURE;
-            task_result.error_message = "delete of subtasks is failed";
-            return task_result;
-        }
+        if(!removeTask(task.value().lock()))
+            return Creator::createRequestTaskResult(ResultType::FAILURE,"delete of subtasks is failed");
+
         storage_.deleteTask(task.value().lock()->getId());
         return subtask_results;
     }
-    else {
-        task_result.result = ResultType::FAILURE;
-        task_result.error_message = "task not found";
-        return task_result;
-    }
+    else
+        return Creator::createRequestTaskResult(ResultType::FAILURE, "task not found");
 }
 
 bool TaskService::removeTask(const std::weak_ptr<FullTask> &task){
@@ -119,9 +102,9 @@ bool TaskService::removeTask(const std::weak_ptr<FullTask> &task){
 std::optional<TaskDTO> TaskService::getTask(TaskID id){
     std::optional<std::weak_ptr<FullTask>> ft = storage_.getTask(id);
     if (ft.has_value())
-        return
-            std::optional<TaskDTO>(TaskConvertor::transferToTaskDTO(ft.value()));
-     else return std::nullopt;
+        return std::optional<TaskDTO>(TaskConvertor::transferToTaskDTO(ft.value()));
+     else
+         return std::nullopt;
 }
 
 
