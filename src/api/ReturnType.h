7fc9b8f97f301bca9c1bb664a6c279b94413838f
illia <illia.dmitriiev@teamdev.com>
2020-08-13 17:36:36 +0300

    created structures

---
+++ src/api/ReturnType.h
@@ -0,0 +1,27 @@
+//
+// Created by Илля on 08.08.2020.
+//
+
+#ifndef TODOLIST_RETURNTYPE_H
+#define TODOLIST_RETURNTYPE_H
+
+#include "TaskID.h"
+#include <string>
+
+enum class ResultType{
+    SUCCESS,
+    FAILURE
+};
+
+struct AddTaskResult{
+    ResultType result;
+    std::optional<TaskID> id;
+    std::string error_message;
+};
+
+struct RequstTaskResult{
+    ResultType result;
+    std::string error_message;
+};
+
+#endif //TODOLIST_RETURNTYPE_H
