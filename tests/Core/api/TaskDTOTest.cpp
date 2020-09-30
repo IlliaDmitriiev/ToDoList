c2f8c0f6789d6d6a8f18ee096366d2b929b3b965
ilya <illia.dmitriiev@teamdev.com>
2020-09-30 18:38:49 +0300

    - updated TaskDTOTest

--- tests/Core/api/TaskDTOTest.cpp
+++ tests/Core/api/TaskDTOTest.cpp
@@ -26,6 +26,39 @@ TEST_F(TaskDTOTest, shouldCreate) {
     EXPECT_EQ(false, dto.isCompleted());
 }
 
+TEST_F(TaskDTOTest, shouldCreate2) {
+    ServiceTaskDTO dto = ServiceTaskDTO::create(
+            boost::gregorian::date(2010, 10, 10),
+            "",
+            "label",
+            TaskPriority::First);
+    EXPECT_EQ(2010, dto.getDate().year());
+    EXPECT_EQ(10, dto.getDate().month());
+    EXPECT_EQ(10, dto.getDate().day());
+    EXPECT_EQ("", dto.getName());
+    EXPECT_EQ("label", dto.getLabel());
+    EXPECT_EQ(TaskPriority::First, dto.getPriority());
+    EXPECT_EQ(false, dto.isCompleted());
+}
+
+TEST_F(TaskDTOTest, shouldCreate3) {
+    ServiceTaskDTO dto = ServiceTaskDTO::create(
+            TaskID::create(47),
+            boost::gregorian::date(2222, 10, 10),
+            "",
+            "",
+            TaskPriority::First,
+            true);
+    EXPECT_EQ(47, dto.getId().getId());
+    EXPECT_EQ(2222, dto.getDate().year());
+    EXPECT_EQ(10, dto.getDate().month());
+    EXPECT_EQ(10, dto.getDate().day());
+    EXPECT_EQ("", dto.getName());
+    EXPECT_EQ("", dto.getLabel());
+    EXPECT_EQ(TaskPriority::First, dto.getPriority());
+    EXPECT_EQ(true, dto.isCompleted());
+}
+
 TEST_F(TaskDTOTest, shouldThrowExeption_1) {
     EXPECT_ANY_THROW(ServiceTaskDTO::create(
             TaskID::create(47),
