7096f5b91cc0643701175962710a66a536ec993e
ilya <illia.dmitriiev@teamdev.com>
2020-10-04 19:58:36 +0300

    - created persister interface

---
+++ src/Core/persistence/Persister.h
@@ -0,0 +1,32 @@
+//
+// Created by ilya on 04.10.2020.
+//
+
+#ifndef TODOLIST_PERSISTER_H
+#define TODOLIST_PERSISTER_H
+
+#include "Core/model/DataInterface.h"
+/*
+ *  Persistence
+ *
+ *  @author: Ilya Dmitriev
+ */
+class Persister{
+
+public:
+    /*
+     * saves all tasks
+     *
+     * @return true, if operation is successful, false otherwise.
+     */
+    virtual bool save() = 0;
+    /*
+     * loads all tasks in system
+     *
+     * @return true, if operation is successful, false otherwise.
+     */
+    virtual bool load() = 0;
+    virtual ~Persister() = default;
+};
+
+#endif //TODOLIST_PERSISTER_H
