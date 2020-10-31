ec839b080ad2b5e1b07a0732411ef532fa28a33e
ilya <illia.dmitriiev@teamdev.com>
2020-10-31 22:57:18 +0200

    - moved mock files

---
+++ tests/Core/Mock/GeneratorMock.h
@@ -0,0 +1,17 @@
+//
+// Created by ilya on 31.10.2020.
+//
+
+#ifndef TODOLIST_GENERATORMOCK_H
+#define TODOLIST_GENERATORMOCK_H
+
+#include <gmock/gmock.h>
+#include "Core/logic/IDGeneratorInterface.h"
+
+class MockGenerator : public IDGeneratorInterface {
+
+public:
+    MOCK_METHOD(TaskID, generateId, (), (override));
+};
+
+#endif //TODOLIST_GENERATORMOCK_H
---
+++ tests/Core/Mock/ViewMock.h
@@ -0,0 +1,22 @@
+//
+// Created by ilya on 31.10.2020.
+//
+
+#ifndef TODOLIST_VIEWMOCK_H
+#define TODOLIST_VIEWMOCK_H
+
+#include <gmock/gmock.h>
+#include "Core/logic/ViewInterface.h"
+
+class MockView : public ViewInterface{
+
+public:
+    MOCK_METHOD(bool, addTask, (const std::weak_ptr<FullTask>&), (override));
+    MOCK_METHOD(bool, deleteTask, (const std::weak_ptr<FullTask>&), (override));
+
+    MOCK_METHOD(std::vector<std::weak_ptr<FullTask>>, getAllTasksByPrior, (),(override));
+    MOCK_METHOD(std::vector<std::weak_ptr<FullTask>>, getTasksForToday, (boost::gregorian::date),(override));
+    MOCK_METHOD(std::vector<std::weak_ptr<FullTask>>, getTasksForWeek, (boost::gregorian::date),(override));
+};
+
+#endif //TODOLIST_VIEWMOCK_H
